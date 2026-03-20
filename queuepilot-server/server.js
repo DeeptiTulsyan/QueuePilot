e37d44a85af6606e83e407121bd321c95eece761
DeeptiTulsyan <deeptitulsyan07@gmail.com>
2026-03-21 00:25:39 +0530

    feat: setup full-stack monorepo

---
+++ queuepilot-server/server.js
@@ -0,0 +1,27 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import connectDB from "./config/db.js";
+import authRoutes from "./routes/authRoutes.js";
+import queueRoutes from "./routes/queueRoutes.js";
+import adminRoutes from "./routes/adminRoutes.js";
+dotenv.config();
+connectDB();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/queue", queueRoutes);
+app.use("/api/admin", adminRoutes);
+app.get("/", (req, res) => {
+  res.send("QueuePilot API Running");
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
