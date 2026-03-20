82b13cf8b98bc5198b2d1698f8e6ea2b655c2c8a
DeeptiTulsyan <deeptitulsyan07@gmail.com>
2026-03-21 00:00:26 +0530

    Add Cancel queue token logic

--- controllers/queueController.js
+++ controllers/queueController.js
@@ -81,3 +81,27 @@ export const getQueueStatus = async (req, res) => {
     res.status(500).json({ message: "Server error" });
   }
 };
+// ================= CANCEL TOKEN =================
+export const cancelToken = async (req, res) => {
+  try {
+    const userId = req.user._id;
+
+    const token = await Token.findOne({
+      user: userId,
+      status: { $in: ["WAITING", "NOW_SERVING"] }
+    });
+
+    if (!token) {
+      return res.status(404).json({ message: "No active token found" });
+    }
+
+    token.status = "CANCELLED";
+    await token.save();
+
+    res.json({ message: "Token cancelled successfully" });
+
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Server error" });
+  }
+};
